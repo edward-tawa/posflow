7d6fd6bb05d4cc1743f43170d4659ea4574a8051
Teddy <teddychinomona@gmail.com>
2025-12-15 11:32:53 +0200

    serializer

--- customers/serializers/customer_service_amount_serializer.py
+++ customers/serializers/customer_service_amount_serializer.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from decimal import Decimal
 
 class AmountSerializer(serializers.Serializer):
-    amount = serializers.DecimalField(max_digits=12, decimal_places=2, min_value=0.01)
+    amount = serializers.DecimalField(max_digits=12, decimal_places=2, min_value=Decimal(0.01))
