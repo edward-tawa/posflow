30de3b48d39b5608fb1ca8e37052f4d6f49b0d2f
Teddy <teddychinomona@gmail.com>
2025-12-27 11:38:35 +0200

    test workflow

--- posflow/test_settings.py
+++ posflow/test_settings.py
@@ -6,6 +6,7 @@ PASSWORD_HASHERS = [
 
 EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
 
+# 3. Faster Database for Testing (SQLite in memory)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
